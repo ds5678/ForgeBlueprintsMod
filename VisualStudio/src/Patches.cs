e03572317cd4373bf4747e8e417070cf8efcda08
ds5678 <jrpritts7@gmail.com>
2021-04-01 21:26:49 -0400

    updated for better performance

--- VisualStudio/src/Patches.cs
+++ VisualStudio/src/Patches.cs
@@ -7,25 +7,18 @@ namespace ForgeBlueprints
 {
     internal static class Patches
     {
-        private const string SCRAP_METAL_NAME = "GEAR_ScrapMetal";
-        private const string COOKING_POT_NAME = "GEAR_CookingPot";
-        private const string PRYBAR_NAME = "GEAR_Prybar";
-        //private const string ARROWHEAD_NAME = "GEAR_ArrowHead";
-		//private const string HEAVYHAMMER_NAME = "GEAR_Hammer";
-
-		//Adjust Item Weights
 		[HarmonyPatch(typeof(GearItem), "ManualUpdate")]
 		private static class ChangeWeightPatch
 		{
 			internal static void Postfix(GearItem __instance)
 			{
-				if (__instance.name == PRYBAR_NAME)
+				if (__instance.name == "GEAR_Prybar")
 				{
 					__instance.m_WeightKG = Settings.options.prybarWeight;
 					__instance.m_Harvest.m_YieldGearUnits[0] = (int)(Settings.options.prybarWeight * 5);
 					__instance.m_Harvest.m_DurationMinutes = ((int)(Settings.options.prybarWeight * 5)) * 15;
 				}
-				if (__instance.name == COOKING_POT_NAME)
+				if (__instance.name == "GEAR_CookingPot")
 				{
 					__instance.m_WeightKG = Settings.options.cookingPotWeight;
 					__instance.m_Harvest.m_YieldGearUnits[0] = (int)(Settings.options.cookingPotWeight * 5);
@@ -34,42 +27,28 @@ namespace ForgeBlueprints
 			}
 		}
 
-		//Change Item Prefab Weights
-		[HarmonyPatch(typeof(GameManager), "Update")]
-		private static class ChangePrefabs
-		{
-			internal static void Postfix()
-			{
-				GetGearItemPrefab(PRYBAR_NAME).m_WeightKG = Settings.options.prybarWeight;
-				GetGearItemPrefab(COOKING_POT_NAME).m_WeightKG = Settings.options.cookingPotWeight;
-			}
-			private static GearItem GetGearItemPrefab(string name) => Resources.Load(name).Cast<GameObject>().GetComponent<GearItem>();
-		}
-
-
 		[HarmonyPatch(typeof(Panel_Crafting), "ItemPassesFilter")]
 		private static class ShowRecipesInToolsSection
 		{
 			internal static void Postfix(Panel_Crafting __instance, ref bool __result, BlueprintItem bpi)
 			{
-				if (bpi?.m_CraftedResult?.name == SCRAP_METAL_NAME && __instance.m_CurrentCategory == Panel_Crafting.Category.Tools)
+				if (bpi?.m_CraftedResult?.name == "GEAR_ScrapMetal" && __instance.m_CurrentCategory == Panel_Crafting.Category.Tools)
 				{
 					__result = true;
 				}
 			}
 		}
 
-		//Update Blueprint when Mod Settings change; will probably cause conflicts with mods that add a cooking pot or prybar blueprint
 		[HarmonyPatch(typeof(Panel_Crafting), "ItemPassesFilter")]
 		private static class UpdateScrapMetalRequirements
 		{
 			internal static void Postfix(BlueprintItem bpi)
 			{
-				if (bpi?.m_CraftedResult?.name == COOKING_POT_NAME)
+				if (bpi?.m_CraftedResult?.name == "GEAR_CookingPot")
 				{
 					bpi.m_RequiredGearUnits[0] = (int)(Settings.options.cookingPotWeight * 5 + 1);
 				}
-				if (bpi?.m_CraftedResult?.name == PRYBAR_NAME )
+				if (bpi?.m_CraftedResult?.name == "GEAR_Prybar")
 				{
 					bpi.m_RequiredGearUnits[0] = (int)(Settings.options.prybarWeight * 5 + 1);
 				}
--- VisualStudio/src/Settings.cs
+++ VisualStudio/src/Settings.cs
@@ -1,4 +1,5 @@
 ﻿using ModSettings;
+using UnityEngine;
 
 namespace ForgeBlueprints
 {
@@ -15,6 +16,18 @@ namespace ForgeBlueprints
         [Description("Default is 1 kg. Affects crafting requirements.")]
         [Slider(0.3f, 2f, 18)]
         public float prybarWeight = 1f;
+
+        protected override void OnConfirm()
+        {
+            base.OnConfirm();
+            ChangePrefabWeights();
+        }
+        internal void ChangePrefabWeights()
+        {
+            GetGearItemPrefab("GEAR_Prybar").m_WeightKG = prybarWeight;
+            GetGearItemPrefab("GEAR_CookingPot").m_WeightKG = cookingPotWeight;
+        }
+        private static GearItem GetGearItemPrefab(string name) => Resources.Load(name).Cast<GameObject>().GetComponent<GearItem>();
     }
     internal static class Settings
     {
@@ -23,6 +36,7 @@ namespace ForgeBlueprints
         {
             options = new ForgeModSettings();
             options.AddToModSettings("Forge Blueprints Mod");
+            options.ChangePrefabWeights();
         }
     }
 }
